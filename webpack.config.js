db77c7976393e770d8fc055bbb3ed2130216f3a0
dennisHu <ddh241@gmail.com>
2019-05-28 11:50:42 -0400

    setup folder structure

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+
+const path = require('path');
+
+module.exports = {
+    context: __dirname,
+    entry: './frontend/reactA06.jsx',
+    output: {
+        path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    devtool: 'eval-source-map',
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['env', 'react']
+                }
+            },
+            {
+                test: /\.node$/,
+                loader: 'node-loader'
+            }
+        ]
+    }
+};
